b91936b7f70f9f7c839c9652014231622b6ac6da
danielryb <59661841+danielryb@users.noreply.github.com>
2025-08-14 13:48:02 +0200

    fix crash on game state change

--- src/patches/play_patches.c
+++ src/patches/play_patches.c
@@ -3,3 +3,7 @@
 RECOMP_HOOK("Player_Update") void on_Player_Update(Actor* thisx, PlayState* play) {
     chaos_is_player_active = true;
 }
+
+RECOMP_HOOK("Player_Destroy") void on_Player_Destroy(Actor* thisx, PlayState* play) {
+    chaos_is_player_active = false;
+}
